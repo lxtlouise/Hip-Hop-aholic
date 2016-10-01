7e2227437ac69cda935714e7ee0f087a1865b6d0
Jaega <szxzlee@163.com>
2016-10-01 15:39:36 +0800

    Introduction of a genre

---
+++ Hip-Hop-aholik/Hip-Hop-aholik/GenreWikiTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  GenreWIkiTableViewCell.swift
+//  hiphop
+//
+//  Created by 13560793366 on 16/9/20.
+//  Copyright © 2016年 13560793366. All rights reserved.
+//
+
+import UIKit
+
+class GenreWikiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var genreWiki: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
