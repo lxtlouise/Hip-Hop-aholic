0196ffd030e2a8587f9da331957f3cabecdd64bf
Jaega <szxzlee@163.com>
2016-10-01 15:19:08 +0800

    Data of Artists and the channels of them

---
+++ Hip-Hop-aholik/Hip-Hop-aholik/ChannelData.swift
@@ -0,0 +1,17 @@
+//
+//  VideoItem.swift
+//  hiphop
+//
+//  Created by 13560793366 on 16/8/20.
+//  Copyright © 2016年 13560793366. All rights reserved.
+//
+
+import Foundation
+
+class ChannelData{
+    var artistName: String!
+    var channelName: String!
+    var thumbnails: NSDictionary!
+    var playlistId: String!
+    var channelUrl: NSURL!
+}
