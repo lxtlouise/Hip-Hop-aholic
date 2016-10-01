7ebe84530332539ba4fb4d9e5dc31189f1a356f0
Jaega <szxzlee@163.com>
2016-10-01 15:28:38 +0800

    A table containing the lists of artists

---
+++ Hip-Hop-aholik/Hip-Hop-aholik/ArtistsTableData.swift
@@ -0,0 +1,37 @@
+//
+//  ArtistsTableData.swift
+//  hiphop
+//
+//  Created by 13560793366 on 16/8/12.
+//  Copyright © 2016年 13560793366. All rights reserved.
+//
+
+import Foundation
+
+var artistsTableData = [
+    ["Afrika Bambaataa","Afroman","ASAP Rocky","A Tribe Called Quest", "AZ"],
+    ["Beastie Boys","B.o.B", "Biggie","Busta Rhymes","Big L","Black Star"],
+    ["Chamillionaire","Chance the Rapper","Chief Keef","Clipping.","Common"],
+    ["De La Soul", "Death Grips","DMX","Drake","Dr. Dre"],
+    ["Eazy-E", "Eminem","Eric B. & Rakim","E-40"],
+    ["Fetty Wap","Flo Rida","Fort Minor","Frank Ocean","Fugees", "Future"],
+    ["Gang Starr","Grandmaster Flash","Gucci Mane","Guru"],
+    ["MC Hammer","Hopsin"],
+    ["Ice Cube","Immortal Technique"],
+    ["Jay Z","J. cole","J Dilla","Juicy J"],
+    ["Kanye west","Kendrick Lamar","DJ Khaled","Kool G Rap","DJ Kool Herc","KRS-One"],
+    ["Lil Wayne","Ludacris","LL Cool J","Lupe Fiasco"],
+    ["Macklemore & Ryan Lewis","Madvillain","Method Man","Missy Elliott","Mobb Deep","Mos Def"],
+    ["Nas","Naughty by Nature","N*E*R*D","Nicki Minaj","N.W.A"],
+    ["OutKast"],
+    ["Pitbull","DJ Premier","Public Enemy","Puff Daddy"],
+    ["Q-Tip"],
+    ["MC Ren","Rick Ross","Run–D.M.C.", "Rakim"],
+    ["Salt-N-Pepa","Scarface","DJ Shadow","Shabazz Palaces","Snoop Dogg"],
+    ["Tech N9ne","The Game","The Lonely Island","The Pharcyde","The Roots","The Sugarhill Gang","Three 6 mafia","T.I."],
+    ["Vanilla Ice"],
+    ["Waka Flocka Flame","Warren G","Wiz Khalifa","Wu-tang Clan"],
+    ["Yelawolf","Yo Gotti","Young Jeezy","Young Thug"],
+    ["2 Chainz","2pac","50cent"]
+]
+
