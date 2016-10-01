e7923a36aec685d93876cb89c955b86905835d7f
Jaega <szxzlee@163.com>
2016-10-01 15:40:43 +0800

    A table of information of genres

---
+++ Hip-Hop-aholik/Hip-Hop-aholik/GenresInfo.swift
@@ -0,0 +1,71 @@
+//
+//  genresTableData.swift
+//  hiphop
+//
+//  Created by 13560793366 on 16/8/12.
+//  Copyright © 2016年 13560793366. All rights reserved.
+//
+
+import Foundation
+
+var genresTableData = [
+    
+    "Hardcore Rap",
+    "Pop Rap",
+    "West Coast Hip Hop",
+    "East Coast Hip Hop",
+    "Jazz Hop",
+    "Comedy Hip Hop",
+    "Conscious Rap",
+    "Alternative Rap",
+    "Gangsta Rap",
+    "Midwest Hip Hop",
+    "Old School Hip Hop",
+    "Political Rap",
+    "Southern Hip Hop",
+    "Turntablism",
+    "Experimental Hip Hop",
+    "Trap Rap"
+]
+
+var artistsOfGenreDictionary = [
+    
+    "Hardcore Rap":["Eminem","DMX","Biggie","Wu-tang Clan","Eric B. & Rakim","Method Man","Run–D.M.C.","Public Enemy","KRS-One","Big L","LL Cool J","Tech N9ne","Immortal Technique","Kool G Rap","Chamillionaire","Ludacris","2pac","Nas","N.W.A","Jay Z", "Rakim"],
+    "Pop Rap":["LL Cool J", "MC Hammer", "Vanilla Ice","Drake","Nicki Minaj","Wiz Khalifa","Flo Rida","Kanye West","Macklemore & Ryan Lewis","B.o.B","Fort Minor","Pitbull","Beastie Boys", "Run–D.M.C."],
+    "West Coast Hip Hop":["N.W.A","Dr. Dre","2pac","Snoop Dogg","Kendrick Lamar","Ice Cube", "The Game","Warren G","MC Ren","Eazy-E"],
+    "East Coast Hip Hop":["Puff Daddy", "Biggie", "Nas", "Wu-tang Clan", "50cent", "Mobb Deep","KRS-One", "AZ", "Jay Z", "ASAP Rocky", "Fugees", "The Roots", "LL Cool J", "Run-D.M.C", "Method Man", "Naughty by Nature", "Eric B. & Rakim", "Afrika Bambaataa", "Public Enemy", "Immortal Technique", "DMX", "Busta Rhymes", "Big L", "J. cole", "DJ Premier", "De La Soul", "Rakim"],
+    "Jazz Hop":["A Tribe Called Quest","Q-Tip","De La Soul","The Roots","Gang Starr","Black Star","Common","DJ Premier","Guru","Eric B. & Rakim","Fugees","The Pharcyde"],
+    "Comedy Hip Hop":["The Lonely Island","Afroman","Macklemore & Ryan Lewis"],
+    "Conscious Rap":["Macklemore & Ryan Lewis","Kendrick Lamar","KRS-One","Immortal Technique","Hopsin","Common","Lupe Fiasco","Grandmaster Flash"],
+    "Alternative Rap":["The Roots","Fort Minor","OutKast","N*E*R*D","De La Soul","Black Star","Chance the Rapper","Mos Def","Frank Ocean","Beastie Boys","Gang Starr","Lupe Fiasco","Macklemore & Ryan Lewis","The Pharcyde","Common","J. cole"],
+    "Gangsta Rap":["N.W.A","Dr. Dre","2pac","Ice Cube","Eazy-E","50cent","Gucci Mane","Scarface","Busta Rhymes","MC Ren"],
+    "Midwest Hip Hop":["Chance the Rapper","Common","Kanye west","Eminem","Tech N9ne"],
+    "Old School Hip Hop":["N.W.A","Run–D.M.C.","Fugees","Eazy-E","Ice Cube","Dr. Dre","Method Man","KRS-One","Eric B. & Rakim","Kool G Rap","MC Ren","Public Enemy","The Sugarhill Gang","Grandmaster Flash","Beastie Boys","A Tribe Called Quest","Afrika Bambaataa","MC Hammer","Naughty by Nature","Salt-N-Pepa","LL Cool J", "Rakim"],
+    "Political Rap":["N.W.A","Public Enemy","Immortal Technique","KRS-One","Mos Def","Lupe Fiasco","Common","Ice Cube"],
+    "Southern Hip Hop":["OutKast","T.I.","Lil Wayne","DJ Khaled","Yelawolf","E-40","2 Chainz","Ludacris","Young Jeezy","Juicy J","ASAP Rocky","Three 6 mafia","Missy Elliott","B.o.B"],
+    "Turntablism":["DJ Premier","DJ Khaled","J Dilla","DJ Kool Herc","DJ Shadow","Grandmaster Flash"],
+    "Experimental Hip Hop":["Madvillain","Shabazz Palaces","Death Grips","Clipping."],
+    "Trap Rap":["Gucci Mane","Young Jeezy","Juicy J","T.I.","Future","Young Thug","Rick Ross","Fetty Wap","Waka Flocka Flame","Chief Keef","ASAP Rocky","Yo Gotti"]
+    
+
+]
+
+var genresWikiDictionary: Dictionary<String, String> = [
+    "Hardcore Rap":"Hardcore_hip_hop",
+    "Pop Rap":"Pop-rap",
+    "West Coast Hip Hop":"West_Coast_hip_hop",
+    "East Coast Hip Hop":"East_Coast_hip_hop",
+    "Jazz Hop":"Jazz_rap",
+    "Comedy Hip Hop":"Comedy_hip_hop",
+    "Conscious Rap":"Political_hip_hop#Conscious_hip_hop",
+    "Alternative Rap":"Alternative_hip_hop",
+    "Gangsta Rap":"Gangsta_rap",
+    "Midwest Hip Hop":"Midwest_hip_hop",
+    "Old School Hip Hop":"Old-school_hip_hop",
+    "Political Rap":"Political_hip_hop",
+    "Southern Hip Hop":"Southern_hip_hop",
+    "Turntablism":"Turntablism",
+    "Experimental Hip Hop":"Experimental_hip_hop",
+    "Trap Rap":"Trap_music"
+
+]
